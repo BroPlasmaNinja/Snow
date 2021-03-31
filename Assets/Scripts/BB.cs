3dcc8c245589fde692d1639bcf35d129bfdde6cd
BroPlasmaNinja <HeadMenlvl100@yandex.ru>
2021-03-31 11:48:05 +0400

    GodJob

--- Assets/Scripts/BB.cs
+++ Assets/Scripts/BB.cs
@@ -11,29 +11,41 @@ public class BB : MonoBehaviour
         up,
         down
     }
+    [SerializeField] GameObject BigCell;
     [SerializeField] dir Dir = dir.right;
-    private bool right, left, up, down;
+    public bool Job;
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            GetComponentInParent<MapController>().OnBorderEnter(Dir);
             Debug.Log(Dir + "\n" + other.name);
-            if(Dir == dir.right)
+            if (Dir == BB.dir.up && !Job)
             {
-
+                GameObject gg = Instantiate(BigCell, new Vector3(transform.position.x, transform.position.y, transform.GetComponentInParent<Transform>().position.z + 100 - 43.5f), new Quaternion(0, 0, 0, 0));
+                //StartCoroutine(wherePos(gg.transform.GetChild(3).position));
+                Job = true;
+                gg.transform.GetChild(3).GetComponent<BB>().Job = true;
             }
-            if(Dir == dir.left)
+            if (Dir == BB.dir.down && !Job)
             {
-
+                GameObject gg = Instantiate(BigCell, new Vector3(transform.position.x, transform.position.y, transform.GetComponentInParent<Transform>().position.z - 100 + 43.5f), new Quaternion(0, 0, 0, 0));
+                //StartCoroutine(wherePos(gg.transform.GetChild(2).position));
+                Job = true;
+                gg.transform.GetChild(2).GetComponent<BB>().Job = true;
             }
-            if(Dir == dir.up)
+            if (Dir == BB.dir.right && !Job)
             {
-
+                GameObject gg = Instantiate(BigCell, new Vector3(transform.GetComponentInParent<Transform>().position.x + 100 - 43.5f, transform.position.y, transform.position.z), new Quaternion(0, 0, 0, 0));
+                //StartCoroutine(wherePos(gg.transform.GetChild(1).position));
+                Job = true;
+                gg.transform.GetChild(1).GetComponent<BB>().Job = true;
             }
-            if(Dir == dir.down)
+            if (Dir == BB.dir.left && !Job)
             {
-
+                GameObject gg = Instantiate(BigCell, new Vector3(transform.GetComponentInParent<Transform>().position.x - 100 + 43.5f, transform.position.y, transform.position.z), new Quaternion(0, 0, 0, 0));
+                //StartCoroutine(wherePos(gg.transform.GetChild(0).position));
+                Job = true;
+                gg.transform.GetChild(0).GetComponent<BB>().Job = true;
             }
         }
     }
--- Assets/Scripts/MapController.cs
+++ Assets/Scripts/MapController.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class MapController : MonoBehaviour
 {
-    [SerializeField] private GameObject BigCell;
     [SerializeField] int MapSize = 20;
     GameObject[,] MyMap;
     [SerializeField] GameObject[] AllCells;
 
     private void OnEnable()
     {
+        Random.InitState((int)Time.realtimeSinceStartup);
         MyMap = new GameObject[MapSize, MapSize];
         Debug.LogWarning("ÕÓ‚˚È Ó·˙ÂÍÚ");
         for (int i = 0; i < MapSize; i++)
@@ -18,46 +18,12 @@ public class MapController : MonoBehaviour
             for (int j = 0; j < MapSize; j++)
             {
                 MyMap[i, j] = gg(AllCells);
-                float x = 5f * (i - MapSize / 2f) + 2.5f;
-                float z = 5f * (j - MapSize / 2f) + 2.5f;
+                float x = 5f * (i - MapSize / 2f) + 2.5f + transform.position.x;
+                float z = 5f * (j - MapSize / 2f) + 2.5f + transform.position.z;
                 Instantiate(MyMap[i, j], new Vector3(x + Random.Range(-2, 2), transform.position.y, z + Random.Range(-2, 2)), new Quaternion(0, 0, 0, 0), transform.GetChild(4));
             }
         }
         Debug.LogWarning(" ŒÕ≈÷");
-
-    }
-
-
-    public void OnBorderEnter(BB.dir Dir)
-    {
-        /*if(Dir == BB.dir.up && !up)
-        {
-            GameObject gg = Instantiate(BigCell, new Vector3 (transform.position.x,transform.position.y,transform.position.z + transform.localScale.z*100), new Quaternion(0, 0, 0, 0));
-            //StartCoroutine(wherePos(gg.transform.GetChild(3).position));
-            up = true;
-            gg.GetComponent<MapController>().down = true;
-        }
-        if(Dir == BB.dir.down && !down)
-        {
-            GameObject gg = Instantiate(BigCell, new Vector3 (transform.position.x,transform.position.y,transform.position.z - transform.localScale.z*100), new Quaternion(0, 0, 0, 0));
-            //StartCoroutine(wherePos(gg.transform.GetChild(2).position));
-            down = true;
-            gg.GetComponent<MapController>().up = true;
-        }
-        if(Dir == BB.dir.right && !right)
-        {
-            GameObject gg = Instantiate(BigCell, new Vector3 (transform.position.x + transform.localScale.x * 100, transform.position.y,transform.position.z), new Quaternion(0, 0, 0, 0));
-            //StartCoroutine(wherePos(gg.transform.GetChild(1).position));
-            left = true;
-            gg.GetComponent<MapController>().left = true;
-        }
-        if(Dir == BB.dir.left && !left)
-        {
-            GameObject gg = Instantiate(BigCell, new Vector3 (transform.position.x - transform.localScale.x * 100, transform.position.y,transform.position.z), new Quaternion(0, 0, 0, 0));
-            //StartCoroutine(wherePos(gg.transform.GetChild(0).position));
-            right = true;
-            gg.GetComponent<MapController>().right = true;
-        }*/
     }
     IEnumerator wherePos(Vector3 pos)
     {
